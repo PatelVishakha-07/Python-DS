e437dcd64a1ab4cca0b7de585d48026b937a6fdc
PatelVishakha-07 <vishakhapatel8270@gmail.com>
2026-06-29 21:42:52 +0530

    msg: program 2 done

--- undo_machine.py
+++ undo_machine.py
@@ -1,13 +1,32 @@
 """ C2) The Undo Machine
-
 You're building a lightweight text editor for a startup. Every time a user types a character, it gets recorded When they press Ctrl+Z (Undo) the last action must be reversed. The editor must support.
 
 type(char)-records a character
-
 . undo() removes the last typed character
-
 getText()returns the current text
+"""
 
-c3) The Smart Printer Queue (Priority Queue)
+class TextEditor:
 
-An office printer handles jobs in order, BUT jobs marked URGENT must be printed before normal jobs Design a system using two queues one for urgent, one for normal-and always drain urgent first  """
+    def __init__(self):
+        self.stack = []
+
+    def writeChar(self):
+        c = input("enter a charcter: ")
+        self.stack.append(c)
+
+    def undo(self):
+        self.stack.pop()
+
+    def getText(self):
+        return "".join(self.stack)
+    
+editor = TextEditor()
+editor.writeChar()
+editor.writeChar()
+editor.writeChar()
+print("Current text: ",editor.getText())
+
+editor.undo()
+editor.writeChar()
+print("Current text: ",editor.getText())
