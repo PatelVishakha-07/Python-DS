57517653faefc938d9b65b624dd16f533db13959
PatelVishakha-07 <vishakhapatel8270@gmail.com>
2026-07-03 23:00:09 +0530

    msg: multiplication as aseries of addition

--- day3/multiplication.py
+++ day3/multiplication.py
@@ -1,10 +1,11 @@
 """ C2) Multiplication as a series of addition
  """
 
-def multiply(n,c, ans):
-    if c==n:
+def multiply(n1, n2, c, ans):
+    if c==n2:
         return ans
-    return multiply(n,c+1, ans+n)
+    return multiply(n1, n2, c+1, ans+n1)
 
-n = int(input("enter a no: "))
-print(multiply(n, 0, 0))
+n1 = int(input("enter a no 1: "))
+n2 = int(input("enter a no 2: "))
+print(multiply(n1,n2, 0, 0))
