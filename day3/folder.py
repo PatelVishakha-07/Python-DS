534bf0cb3aee14a81880631017e62c41e58fb1da
PatelVishakha-07 <vishakhapatel8270@gmail.com>
2026-07-03 21:37:31 +0530

    msg: stairs prg done

---
+++ day3/folder.py
@@ -0,0 +1,2 @@
+""" H2) You're building a file size calculator like Windows Explorer's "Properties". A folder can contain files OR more folders. Calculate the total size of any folder recursively. """
+
--- day3/staircase.py
+++ day3/staircase.py
@@ -1,3 +1,12 @@
-""" H2) You're building a file size calculator like Windows Explorer's "Properties". A folder can contain files OR more folders. Calculate the total size of any folder recursively.
+""" 
 H3) You're climbing a staircase. You can take 1 step or 2 steps at a time. How many distinct ways can you reach the Nth stair? """
 
+def stairs(n):
+    if n <= 1:
+        return 1
+    
+    return stairs(n-1) + stairs(n-2)
+
+n = int(input("enter a no: "))
+
+print(stairs(n))
